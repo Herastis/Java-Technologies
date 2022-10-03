85beeadcd4d3a791a04503705b8bb18b437f8f07
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2022-10-04 01:23:50 +0300

    Fix permutations

--- Lab1/Lab1/src/main/java/WordServlet.java
+++ Lab1/Lab1/src/main/java/WordServlet.java
@@ -31,35 +31,33 @@ public class WordServlet extends HttpServlet {
         PrintWriter writer = response.getWriter();
 
         String htmlRespone = "<html>";
-        htmlRespone += "<h2>Ordered list: [";
+        htmlRespone += "<h2>1)Ordered list: [";
 
         htmlRespone += chars[0];
         for (int i = 0; i < chars.length; ++i) {
             htmlRespone += ", " + chars[i];
         }
 
-        htmlRespone += "]. </h2> </br>";
-//        htmlRespone += "<h2>Permutations: " + getPermutation(word, "", size) + "</h2>";
+        htmlRespone += "]. </h2>";
+        htmlRespone += "<h2>2)Permutations: " + getPermutation(word, "", size) + "</h2>";
         htmlRespone += "</html>";
         writer.println(htmlRespone);
     }
 
-//    private static String permutation(String word, String prefix, int size) {
-//        if (prefix.length() != size) {
-//            for (int i = 0; i < word.length(); i++) {
-//                return permutation(word, prefix + word.charAt(i), size);
-//            }
-//        }
-//        return prefix;
-//    }
-//
-//    private static ArrayList<String> getPermutation(String word, String prefix, int size) {
-//        ArrayList<String> res = new ArrayList<>();
-//        for (int i = 0; i < word.length(); i++) {
-//            res.add(permutation(word, prefix + word.charAt(i), size));
-//        }
-//
-//        Collections.sort(res);
-//        return res;
-//    }
+    private static void permutation(String word, String prefix, int size, ArrayList<String> res) {
+        if (prefix.length() == size) {
+             res.add(prefix);
+         } else {
+             for (int i = 0; i < word.length(); i++) {
+                 permutation(word, prefix + word.charAt(i), size, res);
+             }
+         }
+    }
+
+    private static ArrayList<String> getPermutation(String word, String prefix, int size) {
+        ArrayList<String> res = new ArrayList<>();
+        permutation(word, prefix, size, res);
+        Collections.sort(res);
+        return res;
+    }
 }
