6e127fd132a510b0d230f2c7ba49afea441e2777
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2022-10-08 22:46:40 +0300

    Permutation of length size feature

--- Lab1/Lab1/src/main/java/WordServlet.java
+++ Lab1/Lab1/src/main/java/WordServlet.java
@@ -21,41 +21,53 @@ public class WordServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        PrintWriter writer = response.getWriter();
         String word = request.getParameter("word");
         Integer size = Integer.valueOf(request.getParameter("size"));
 
+        //Letters of word in ordered list
         char[] chars = word.toCharArray();
         Arrays.sort(chars);
 
-        System.out.println("Ordered list: " + Arrays.toString(chars));
-        PrintWriter writer = response.getWriter();
-
         String htmlRespone = "<html>";
-        htmlRespone += "<h2>1)Ordered list: [";
 
+        htmlRespone += "<h2>Ordered list: [";
         htmlRespone += chars[0];
-        for (int i = 0; i < chars.length; ++i) {
+        for (int i = 1; i < chars.length; ++i) {
             htmlRespone += ", " + chars[i];
         }
+        htmlRespone += "]. </h2> </br>";
 
-        htmlRespone += "]. </h2>";
-        htmlRespone += "<h2>2)Permutations: " + getPermutation(word, "", size) + "</h2>";
-        htmlRespone += "</html>";
+        //Returns all the permutations of length size
+        if (size > word.length()) {
+            htmlRespone += "<h2>Size of permutation is bigger than the word size!<h2>";
+        } else {
+            htmlRespone += "<h2>Permutations: " + getPermutation(word, "", size) + "</h2>";
+            htmlRespone += "</html>";
+        }
+
         writer.println(htmlRespone);
     }
 
     private static void permutation(String word, String prefix, int size, ArrayList<String> res) {
         if (prefix.length() == size) {
-             res.add(prefix);
-         } else {
-             for (int i = 0; i < word.length(); i++) {
-                 permutation(word, prefix + word.charAt(i), size, res);
-             }
-         }
+            res.add(prefix);
+        } else {
+            for (int i = 0; i < word.length(); i++) {
+                permutation(word.substring(0, i)
+                        + word.substring(i + 1, word.length()), prefix + word.charAt(i), size, res);
+            }
+        }
     }
 
     private static ArrayList<String> getPermutation(String word, String prefix, int size) {
+        //Array with permutations
         ArrayList<String> res = new ArrayList<>();
+        if (size == 0) {
+            // If the size is 0 (default), it will return all the sequences.
+            permutation(word, prefix, word.length(), res);
+        }
+
         permutation(word, prefix, size, res);
         Collections.sort(res);
         return res;
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Java-Technologies
+
+Lab 1
+
+Compulsory (1p)
+1) Create a servlet that receives a word and returns an HTML page containing the letters of that word presented as an ordered list. ✔️
+
+Homework (2p)
+2) It receives an integer as a parameter, called size, and it returns all the permutations of length size of the given letters. If the size is 0 (default), it will return all the sequences. ✔️
