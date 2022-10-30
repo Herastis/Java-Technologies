d12b8ce52b26ff7dad6ba82caeebec6d8b50eea3
Tudor Tomescu <Tudor.Tomescu@centric.eu>
2022-10-30 11:51:06 +0200

    Initial commit on Lab3

---
+++ SportsTournamentScheduling/src/main/java/com/mycompany/lab1/domain/entities/City.java
@@ -0,0 +1,15 @@
+package com.mycompany.lab1.domain.entities;
+
+public class City {
+
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
